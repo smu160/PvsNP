de35b763d64c2cb877087def5a6972a654317fb5
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-02-27 08:04:04 -0500

    Made print statement give more details about inconsistencies

--- TestDataFrames.py
+++ TestDataFrames.py
@@ -5,19 +5,22 @@ Created on Fri Feb 16 21:14:44 2018
 
 @author: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
 """
+import numba
 
+
+@numba.jit
 def isDataConsistent(python_DS, MATLAB_DS):
     """
     Checks two dataframes for inconsistencies
     """
     inconsistency = False
-    for i in range(0, python_DS.columns.size):
-        for j in range(0, python_DS[0].size):
-            if int(python_DS[i][j]) != int(MATLAB_DS[i][j]):
+    for column in python_DS:
+        for j in range(0, len(python_DS[column])):
+            if int(python_DS[column][j]) != int(MATLAB_DS[column][j]):
                 inconsistency = True
-                print("Inconsistency found at: ({}, {}) ... python_DS[i][j] = {}, test[i][j] = {}".format(i, j, int(python_DS[i][j]), int(MATLAB_DS[i][j])))
+                print("Inconsistency found at: ({}, {}) ... python_DS[{}][{}] = {}, test[{}][{}] = {}".format(column, j, column, j, python_DS[column][j], column, j, MATLAB_DS[column][j]))
 
-    if inconsistency == False:
+    if inconsistency is False:
         return "No inconsistencies found"
     else:
         return "Inconsistencies found!"
