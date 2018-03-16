2d8c61702f4c3f214229329156ed9826d728a632
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-03-16 01:34:30 -0400

    Fixed print statements

--- TestDataFrames.py
+++ TestDataFrames.py
@@ -5,10 +5,7 @@ Created on Fri Feb 16 21:14:44 2018
 
 @author: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
 """
-import numba
 
-
-@numba.jit
 def isDataConsistent(python_DS, MATLAB_DS):
     """
     Checks two dataframes for inconsistencies
@@ -18,7 +15,7 @@ def isDataConsistent(python_DS, MATLAB_DS):
         for j in range(0, len(python_DS[column])):
             if int(python_DS[column][j]) != int(MATLAB_DS[column][j]):
                 inconsistency = True
-                print("Inconsistency found at: ({}, {}) ... python_DS[{}][{}] = {}, test[{}][{}] = {}".format(column, j, column, j, python_DS[column][j], column, j, MATLAB_DS[column][j]))
+                print("Inconsistency found at: ({}, {}) ... python_DS[{}][{}] = {}, MATLAB_DS[{}][{}] = {}".format(column, j, column, j, python_DS[column][j], column, j, MATLAB_DS[column][j]))
 
     if inconsistency is False:
         return "No inconsistencies found"
