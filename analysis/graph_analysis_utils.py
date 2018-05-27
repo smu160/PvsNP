cff8dfe4b0890d7ef8993715c7a27d661d86b178
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-05-27 00:06:09 -0400

    Overhauled the NeuronNetwork class

--- analysis/graph_analysis_utils.py
+++ analysis/graph_analysis_utils.py
@@ -1,7 +1,6 @@
 """
-This module contains all the functions that are needed
-for quickly generating and plotting visualizations of
-networks.
+This module contains the NeuronNetwork class, which is to be used for quickly
+generating and plotting visualizations of neuron networks.
 """
 
 import matplotlib.pyplot as plt
@@ -10,40 +9,55 @@ import networkx as nx
 from networkx.algorithms.approximation import clique
 import analysis.analysis_utils as au
 
-class neuron_network(object):
-    
+class NeuronNetwork(object):
+    """
+    This is a wrapper class for the NetworkX graph data structure.
+    Use this class in order to conduct graph theory analysis on
+    networks of neurons that were recorded using Calcium Imaging.
+    """
     def __init__(self, dataframe):
-       self.graph = self.create_graph(dataframe)
+        self.network = self.create_graph(dataframe)
+        self.neurons = list(self.network.nodes())
+        self.mean_degree_centrality = self.compute_mean_degree_cent()
+        self.connection_density = self.compute_connection_density()
+        self.global_efficiency = nx.global_efficiency(self.network)
+        self.local_efficiency = nx.local_efficiency(self.network)
+        self.clustering_coefficient = nx.average_clustering(self.network)
+        self.max_clique_size = self.compute_max_clique_size()
+        self.mean_clique_size = self.compute_mean_clique_size()
+        # self.mean_betw_centrality = self.compute_mean_betw_cent()
+        # self.mean_katz_centrality = self.compute_mean_katz_cent()
+        # self.mean_load_centrality = self.compute_mean_load_cent()
 
     def create_graph(self, dataframe):
         """Wrapper function for creating a NetworkX graph
 
         Each individual column of the provided DataFrame will be represented by
         a single node in the graph. Each pair of correlated nodes (neurons)
-        will be connected by an edge, where the edge will receive a weight of 
+        will be connected by an edge, where the edge will receive a weight of
         the specific correlation coefficient of those two nodes.
 
         Args:
-            dataframe: DataFrame 
-            
+            dataframe: DataFrame
+
                 a pandas DataFrame that contains the data to be represented
                 with a NetworkX graph
 
         Returns:
-            G: graph
-            
-                a NetworkX graph of the neuronal network
+            graph: NetworkX graph
+
+                a graph of the neuronal network
         """
-        G = nx.Graph()
-        G.add_nodes_from(dataframe.columns)
+        graph = nx.Graph()
+        graph.add_nodes_from(dataframe.columns)
         corr_pairs = au.find_correlated_pairs(dataframe, correlation_coeff=0.3)
 
         for key in corr_pairs:
-            G.add_edge(key[0], key[1], weight=round(corr_pairs[key], 3))
+            graph.add_edge(key[0], key[1], weight=round(corr_pairs[key], 3))
 
-        return G
+        return graph
 
-    def create_random_graph(dataframe):
+    def gen_rand_graph(self, dataframe):
         """Generates a random NetworkX graph
 
         Each individual column of the provided DataFrame will be represented by
@@ -52,63 +66,71 @@ class neuron_network(object):
         edges will be added between random pairs of nodes (neurons) in the graph.
 
         Args:
-            dataframe: DataFrame 
-                
+            dataframe: DataFrame
+
                 the pandas DataFrame to use as a basis for the random graph
-        
-        Returns: 
-            G: graph
+
+        Returns:
+            graph: NetworkX graph
                 a NetworkX graph of the neuronal network
         """
-        G = nx.Graph()
-        G.add_nodes_from(dataframe.columns)
+        graph = nx.Graph()
+        graph.add_nodes_from(dataframe.columns)
         corr_pairs = au.find_correlated_pairs(dataframe, correlation_coeff=0.3)
 
-        # Connect a len(correlated_pairs_dict) amount of random edges between 
+        # Connect a len(correlated_pairs_dict) amount of random edges between
         # all nodes in the random graph
         end_node1 = np.random.randint(1, len(dataframe.columns)+1)
-        end_node2 = np.random.randint(1, len(dataframe.columns)+1) 
-        for i in range(len(corr_pairs)):
-            G.add_edge(end_node1, end_node2)
+        end_node2 = np.random.randint(1, len(dataframe.columns)+1)
+        for _ in range(len(corr_pairs)):
+            graph.add_edge(end_node1, end_node2)
 
-        return G
+        return graph
 
-    def plot_network(self, **kwargs):
+    def plot_with_spring_layout(self, **kwargs):
         """A wrapper function for plotting a NetworkX graph
 
         This function will draw a provided NetworkX graph using the spring
         layout algorithm.
 
         Args:
-            G: graph
-                
-                the NetworkX graph to be plotted
+            node_size: int, optional
+
+                the size of the plotted neurons in the network.
+
+            node_color: str, optional
+
+                the color of the neurons to be plotted.
+
+            fontsize: int, optional
+
+                the size of the font in the plotted neurons.
         """
 
         # positions for all nodes
-        pos = nx.spring_layout(self.graph, weight="weight")
+        pos = nx.spring_layout(self.network, weight="weight")
 
         # Size of the plot
-        plt.figure(figsize=kwargs.get("figsize", (35,35)))
+        plt.figure(figsize=kwargs.get("figsize", (35, 35)))
 
         # nodes
         node_size = kwargs.get("node_size", 1000)
         color = kwargs.get("node_color", "pink")
-        nx.draw_networkx_nodes(self.graph, pos, node_size=node_size, node_color=color);
+        nx.draw_networkx_nodes(self.network, pos, node_size=node_size, node_color=color)
 
-        edges, weights = zip(*nx.get_edge_attributes(self.graph, "weight").items())
+        edges, weights = zip(*nx.get_edge_attributes(self.network, "weight").items())
 
         # edges
-        nx.draw_networkx_edges(self.graph, pos, width=3.0, edge_color=weights, edge_cmap=plt.cm.YlGnBu);
+        nx.draw_networkx_edges(self.network, pos, width=3.0, edge_color=weights, edge_cmap=plt.cm.YlGnBu)
 
-        labels = nx.get_edge_attributes(self.graph, "weight")
-        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=labels)
+        labels = nx.get_edge_attributes(self.network, "weight")
+        nx.draw_networkx_edge_labels(self.network, pos, edge_labels=labels)
 
         # labels
         font_size = kwargs.get("font_size", 15)
-        nx.draw_networkx_labels(self.graph, pos, font_size=font_size, edge_labels=labels)
+        nx.draw_networkx_labels(self.network, pos, font_size=font_size, edge_labels=labels)
 
-        plt.axis("off");
+        plt.axis("off")
 
         save_to_file = kwargs.get("save", False)
         if save_to_file:
@@ -116,68 +138,75 @@ class neuron_network(object):
             file_format = kwargs.get("format", "PNG")
             plt.savefig(title, format=file_format)
 
-        plt.show();
+        plt.show()
 
+    def plot_by_true_pos(self):
+        """Plots the network of neurons as they look in reality.
+        """
+        pass
+
+    # TODO: Find out if this is still needed and fix this function
     def generate_random_graph(self):
-
+        """Generates a random graph based on the graph
+        """
         # positions for all nodes
-        pos = nx.spring_layout(random_graph, weight='weight')
+        pos = nx.spring_layout(self.network, weight='weight')
 
-        plt.figure(figsize=(15,15))
+        plt.figure(figsize=(15, 15))
 
         # nodes
-        nx.draw_networkx_nodes(random_graph, pos, node_size=700, node_color='lightblue');
+        nx.draw_networkx_nodes(self.network, pos, node_size=700, node_color='lightblue')
 
         # edges
-        nx.draw_networkx_edges(random_graph, pos, width=1.0);
+        nx.draw_networkx_edges(self.network, pos, width=1.0)
 
-        labels = nx.get_edge_attributes(random_graph, 'weight')
-        nx.draw_networkx_edge_labels(random_graph, pos, edge_labels=labels)
+        labels = nx.get_edge_attributes(self.network, 'weight')
+        nx.draw_networkx_edge_labels(self.network, pos, edge_labels=labels)
 
         # labels
-        nx.draw_networkx_labels(random_graph, pos, font_size=15, edge_labels=labels)
+        nx.draw_networkx_labels(self.network, pos, font_size=15, edge_labels=labels)
 
-        plt.axis('off');
-        plt.show();
+        plt.axis('off')
+        plt.show()
 
     def compute_connection_density(self):
-        n = len(list(self.graph.nodes()))
-        return len(list(self.graph.edges())) / ((n * (n-1)) / 2)
+        num_of_neurons = len(self.neurons)
+        possible_num_of_edges = (num_of_neurons * (num_of_neurons-1)) / 2
+        return len(list(self.network.edges())) / possible_num_of_edges
 
     def compute_mean_betw_cent(self):
-        graph_centrality = nx.betweenness_centrality(self.graph, weight="weight")
+        graph_centrality = nx.betweenness_centrality(self.network, weight="weight")
         return np.mean(list(graph_centrality.values()))
 
     def compute_mean_degree_cent(self):
-        graph_centrality = nx.degree_centrality(self.graph)
+        graph_centrality = nx.degree_centrality(self.network)
         return np.mean(list(graph_centrality.values()))
 
     def compute_mean_eigen_cent(self):
-        graph_centrality = nx.eigenvector_centrality(self.graph, weight="weight")
+        graph_centrality = nx.eigenvector_centrality(self.network, weight="weight")
         return np.mean(list(graph_centrality.values()))
 
     def compute_mean_katz_cent(self):
-        graph_centrality = nx.katz_centrality(self.graph)
+        graph_centrality = nx.katz_centrality(self.network)
         return np.mean(list(graph_centrality.values()))
 
     def compute_mean_load_cent(self):
-        graph_centrality = nx.load_centrality(self.graph, weight="weight")
+        graph_centrality = nx.load_centrality(self.network, weight="weight")
         return np.mean(list(graph_centrality.values()))
 
     def compute_max_clique_size(self):
         "https://en.wikipedia.org/wiki/Clique_(graph_theory)#Definitions"
-        return len(clique.max_clique(self.graph))
+        return len(clique.max_clique(self.network))
 
     def compute_mean_clique_size(self):
         """Computes the mean clique size of a given graph
 
-            Args:
-                G: a NetworkX graph
-
             Returns:
-                mean: the mean clique size of the given NetworkX graph, G
+                mean:
+
+                    the mean clique size of the given NetworkX graph, G
         """
-        all_cliques = nx.enumerate_all_cliques(self.graph)
+        all_cliques = nx.enumerate_all_cliques(self.network)
 
         size = 0
         running_sum = 0
