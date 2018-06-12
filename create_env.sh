4c75429950dee877b737dc3c4193a3f69b3ccd5c
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-06-12 18:38:45 +0300

    No longer using environment.yml file to create conda env

--- create_env.sh
+++ create_env.sh
@@ -7,15 +7,21 @@ printf "\e[36;1mNow updating Anaconda... \e[0m \n"
 conda update --all
 
 printf "\e[36;1mCreating an environment for Hen Lab code base... \e[0m \n"
-conda env create -f environment.yml 
+conda create --name henlabenv --clone base 
 
 printf "\e[36;1mActivating newly created conda environment... \e[0m \n"
 source activate henlabenv
+
+printf "\e[36;1mInstalling plotly... \e[0m \n"
+conda install -c anaconda plotly 
 
 printf "\e[36;1mInstalling Node.js, requirement for plotly... \e[0m \n"
 conda install -c conda-forge nodejs
 
 printf "\e[36;1mInstalling plotly extension for JupyterLab... \e[0m \n"
 jupyter labextension install @jupyterlab/plotly-extension
+
+printf "\e[36;1mUpdating Anaconda once more... \e[0m \n"
+conda update --all
 
 printf "\e[36;1mEnvironment creation and dependency installation complete! \e[0m \n"
