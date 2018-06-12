134d7d5d2a3f3882bab8a419036d850a7f181985
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-06-12 14:36:49 +0300

    First commit of bash scipt and shared environment file

---
+++ create_env.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+printf "\e[36;1mNow updating conda... \e[0m \n"
+conda update -n root conda
+
+printf "\e[36;1mNow updating Anaconda... \e[0m \n"
+conda update --all
+
+printf "\e[36;1mCreating an environment for Hen Lab code base... \e[0m \n"
+conda env create -f environment.yml 
+
+printf "\e[36;1mActivating newly created conda environment... \e[0m \n"
+source activate henlabenv2
+
+printf "\e[36;1mInstalling Node.js, requirement for plotly... \e[0m \n"
+conda install -c conda-forge nodejs
+
+printf "\e[36;1mInstalling plotly extension for JupyterLab... \e[0m \n"
+jupyter labextension install @jupyterlab/plotly-extension
+
+printf "\e[36;1mEnvironment creation and dependency installation complete! \e[0m \n"
