970da317faff109cc36fba5c98157f150c6d0bdb
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-11-01 04:05:13 -0400

    Added second video player to GUI

--- gui/video_player.py
+++ gui/video_player.py
@@ -3,9 +3,10 @@ import wx.media
 import os
 
 import matplotlib       # Provides the graph figures
-matplotlib.use('WXAgg') # matplotlib needs a GUI (layout), we use wxPython
+# matplotlib.use('WXAgg') # matplotlib needs a GUI (layout), we use wxPython
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigCanvas
+from multiprocessing import Process
 
 class StaticText(wx.StaticText):
     """
@@ -29,15 +30,17 @@ class VideoPanel(wx.Panel):
         # Create some controls
         try:
             self.mc = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER, size=(350,350))
-        
+            self.mc2 = wx.media.MediaCtrl(self, style=wx.SIMPLE_BORDER, size=(350,350))
         except NotImplementedError:
             self.Destroy()
             raise
 
         self.Bind(wx.media.EVT_MEDIA_LOADED, self.OnMediaLoaded)
 
-        load_file_button = wx.Button(self, -1, "Load File")
-        self.Bind(wx.EVT_BUTTON, self.OnLoadFile, load_file_button)
+        load_vid1_button = wx.Button(self, -1, "Load Video 1")
+        self.Bind(wx.EVT_BUTTON, self.OnLoadFile, load_vid1_button)
+        load_vid2_button = wx.Button(self, -1, "Load Video 2")
+        self.Bind(wx.EVT_BUTTON, self.OnLoadFile, load_vid2_button)
 
         self.play_button = wx.Button(self, -1, "Play")
         self.Bind(wx.EVT_BUTTON, self.OnPlay, self.play_button)
@@ -52,49 +55,56 @@ class VideoPanel(wx.Panel):
         slider.SetMinSize((150, -1))
         self.Bind(wx.EVT_SLIDER, self.OnSeek, slider)
 
-        self.st_size = StaticText(self, -1, size=(100,-1))
-        self.st_len  = StaticText(self, -1, size=(100,-1))
-        self.st_pos  = StaticText(self, -1, size=(100,-1))
+        # self.st_size = StaticText(self, -1, size=(100,-1))
+        # self.st_len  = StaticText(self, -1, size=(100,-1))
+        # self.st_pos  = StaticText(self, -1, size=(100,-1))
 
         # setup the layout
-        sizer = wx.GridBagSizer(5,5)
-        sizer.Add(self.mc, (0,1), span=(5,1))#, flag=wx.EXPAND)
-        sizer.Add(load_file_button, (5,0))
-        sizer.Add(self.play_button, (6,0))
-        sizer.Add(stop_button, (7,0))
-        sizer.Add(slider, (6,1), flag=wx.EXPAND)
-        sizer.Add(self.st_size, (1, 5))
-        sizer.Add(self.st_len,  (2, 5))
-        sizer.Add(self.st_pos,  (3, 5))
+        sizer = wx.GridBagSizer(10, 10)
+        sizer.Add(self.mc, (1, 1), span=(5, 1)) #, flag=wx.EXPAND)
+        sizer.Add(self.mc2, (10, 1), span=(5, 1))
+        sizer.Add(load_vid1_button, (8, 0))
+        sizer.Add(load_vid2_button, (8, 1))
+        sizer.Add(self.play_button, (6, 0))
+        sizer.Add(stop_button, (7, 0))
+        sizer.Add(slider, (6, 1), flag=wx.EXPAND)
+        # sizer.Add(self.st_size, (1, 5))
+        # sizer.Add(self.st_len, (2, 5))
+        # sizer.Add(self.st_pos, (3, 5))
         self.SetSizer(sizer)
 
-        # wx.CallAfter(self.DoLoadFile, os.path.abspath("~/Desktop/Drd87_EPM_bgremoved_demo.avi"))
+        self.init_contours()
+
         if timer:
             self.timer = timer
         else:
             self.timer = wx.Timer(self)
-        
+
         self.Bind(wx.EVT_TIMER, self.OnTimer)
         self.timer.Start(100)
 
     def OnLoadFile(self, evt):
+        button = evt.GetEventObject().GetLabel()
         dlg = wx.FileDialog(self, message="Choose a media file", defaultDir=os.getcwd(), defaultFile="", style=wx.FD_OPEN | wx.FD_CHANGE_DIR)
 
         if dlg.ShowModal() == wx.ID_OK:
             path = dlg.GetPath()
-            self.DoLoadFile(path)
+            if button == "Load Video 1":
+                self.DoLoadFile(path, self.mc)
+            elif button == "Load Video 2":
+                self.DoLoadFile(path, self.mc2)
 
         dlg.Destroy()
 
-    def DoLoadFile(self, path):
+    def DoLoadFile(self, path, media_ctrl):
         self.play_button.Disable()
 
-        if not self.mc.Load(path):
+        if not media_ctrl.Load(path):
             wx.MessageBox("Unable to load {}".format(path), "ERROR", wx.ICON_ERROR | wx.OK)
         else:
-            self.mc.GetBestSize()
+            media_ctrl.GetBestSize()
             self.GetSizer().Layout()
-            self.slider.SetRange(0, self.mc.Length())
+            self.slider.SetRange(0, media_ctrl.Length())
 
     def OnMediaLoaded(self, evt):
         self.play_button.Enable()
@@ -108,10 +118,12 @@ class VideoPanel(wx.Panel):
             self.slider.SetRange(0, self.mc.Length())
             if not self.playing and self.coupled_graph:
                 self.mc.Play()
+                self.mc2.Play()
                 self.playing = True
                 self.coupled_graph.paused = False
             else:
                 self.mc.Pause()
+                self.mc2.Pause()
                 self.playing = False
                 self.coupled_graph.paused = True
 
@@ -126,6 +138,7 @@ class VideoPanel(wx.Panel):
 
     def OnStop(self, evt):
         self.mc.Stop()
+        self.mc2.Stop()
         self.playing = False
         self.on_update_play_button(evt)
 
@@ -134,14 +147,16 @@ class VideoPanel(wx.Panel):
             self.coupled_graph.reset_plot()
 
     def OnSeek(self, evt):
+        self.OnPlay(None)
         offset = self.slider.GetValue()
-        # print(offset)
         self.mc.Seek(offset)
-        self.redraw += 1
-        if self.coupled_graph and self.redraw == 1:
-            self.redraw = 0
-            self.coupled_graph.datagen.index = offset // 100
-            self.coupled_graph.draw_plot()
+        self.mc2.Seek(offset)
+        if self.coupled_graph:
+           self.coupled_graph.datagen.index = offset // 100
+           self.coupled_graph.draw_plot()
+
+        self.OnPlay(None)
+
 
     def OnTimer(self, evt):
         offset = self.mc.Tell()
@@ -151,12 +166,25 @@ class VideoPanel(wx.Panel):
         # self.st_pos.SetLabel("position: {}".format(offset))
         if self.coupled_graph:
             self.coupled_graph.on_redraw_timer(offset//100)
-            # self.coupled_graph.datagen.index = offset // 100
-            # print("plot(s) index: {}, video time: {}".format(self.coupled_graph.datagen.index, offset//100))
+            self.coupled_graph.datagen.index = offset // 100
 
     def ShutdownDemo(self):
         self.timer.Stop()
         del self.timer
+
+    def init_contours(self):
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Centre()
+        self.Show(True)
+
+    def on_paint(self, e):
+        dc = wx.PaintDC(self)
+
+        # Red non-filled circle
+        dc.SetPen(wx.Pen("red"))
+        dc.SetBrush(wx.Brush("red", wx.TRANSPARENT))
+        dc.DrawCircle(50, 100, 10)
+
 
 if __name__ == "__main__":
     app = wx.App(False)
