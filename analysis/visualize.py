9052a41c98d27a3dde515671e7b5f2e763e5ef05
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-11-08 20:54:25 -0500

    Implemented methods to generate and plot heatmaps.

--- analysis/visualize.py
+++ analysis/visualize.py
@@ -1,7 +1,67 @@
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+from scipy.ndimage.filters import gaussian_filter
+
+def generate_heatmap(x, y, sigma, bins=(50, 50)):
+    """Generates a heatmap
+
+    Args:
+        x: array
+
+        y: array
+
+        sigma: int
+
+        bins: tuple, optional
+
+    Returns:
+        heatmap:
+
+        extent:
+    """
+
+    heatmap, xedges, yedges = np.histogram2d(x, y, bins=bins)
+    heatmap = gaussian_filter(heatmap, sigma=sigma)
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    return heatmap.T, extent
+
+def plot_heatmap(x, y, sigma, **kwargs):
+    """ Plots a heatmap
+
+    Wrapper function for matplotlib, numpy, and scipy that
+    plots a heatmap with the capability to smooth the heatmap.
+
+    Args:
+        x: array
+
+        y: array
+
+        sigma: int
+
+        bins: tuple, optional
+
+        figsize: tuple, optional
+
+        title: str, optional
+
+        dpi: int, optional
+    """
+    title = kwargs.get("title", "Title Goes Here")
+    bins = kwargs.get("bins", (50, 50))
+
+    heatmap, extent = generate_heatmap(x, y, sigma, bins=bins)
+    plt.figure(figsize=kwargs.get("figsize", (10, 10)))
+    plt.imshow(heatmap, origin="lower", cmap=cm.jet)
+    plt.title("{}".format(title))
+    plt.axis("off")
+
+    dpi = kwargs.get("dpi", 300)
+    if kwargs.get("savefig", False):
+        plt.savefig("{}.png".format(title), dpi=dpi);
 
 def pie_chart(sizes, *labels, **kwargs):
-    """wrapper method for matplotlib's pie chart
+    """Wrapper method for matplotlib's pie chart.
 
     The slices will be ordered and plotted counter-clockwise.
 
