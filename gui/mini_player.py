896eea7e1e3863425b17b1f4f4f3f7b8fc10d2c8
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-12-28 22:38:19 -0500

    Rewrote with PyQt5 instead of wxPython

--- gui/mini_player.py
+++ gui/mini_player.py
@@ -2,143 +2,133 @@
 """
 
 import os
+import sys
 import queue
 import platform
 
-import wx
+from PyQt5 import QtWidgets, QtGui, QtCore
 import vlc
 from client import Client
 
-class MiniPlayer(wx.Frame):
-    """Stripped-down PyVLC video player class to sync with a main video player.
+class MiniPlayer(QtWidgets.QMainWindow):
+    """Stripped-down PyQt5-based media player class to sync with another video.
     """
 
-    def __init__(self, title, q):
-        wx.Frame.__init__(self, None, -1, title, pos=wx.DefaultPosition, size=(550, 500))
+    def __init__(self, data_queue, master=None):
+        QtWidgets.QMainWindow.__init__(self, master)
+        self.setWindowTitle("Mini Player")
 
-        # Menu Bar
-        # File Menu
-        self.frame_menubar = wx.MenuBar()
-        self.file_menu = wx.Menu()
-        self.file_menu.Append(1, "&Open", "Open from file..")
-        self.file_menu.AppendSeparator()
-        self.file_menu.Append(2, "&Close", "Quit")
-        self.file_menu.AppendSeparator()
-        self.Bind(wx.EVT_MENU, self.on_open, id=1)
-        self.Bind(wx.EVT_MENU, self.on_exit, id=2)
-        self.frame_menubar.Append(self.file_menu, "File")
-        self.SetMenuBar(self.frame_menubar)
-
-        # Panels
-        # The first panel holds the video and it's all black
-        self.videopanel = wx.Panel(self, -1)
-        self.videopanel.SetBackgroundColour(wx.BLACK)
-
-        # Put everything together
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(self.videopanel, 1, flag=wx.EXPAND)
-        self.SetSizer(sizer)
-        self.SetMinSize((350, 300))
-
-        # VLC player controls
+        # Create a basic vlc instance
         self.instance = vlc.Instance()
-        self.player = self.instance.media_player_new()
 
-        self.on_open(None)
-        self.data_q = q
+        self.media = None
 
-        # Create the timer, which updates the video by reading from the queue
-        self.timer = wx.Timer(self)
-        self.timer.Start(100)
-        self.Bind(wx.EVT_TIMER, self.on_timer, self.timer)
+        # Create an empty vlc media player
+        self.mediaplayer = self.instance.media_player_new()
+
+        self.init_ui()
+        self.open_file()
+        self.data_queue = data_queue
 
         try:
-            self.current_time = self.data_q.get(block=False)
+            self.current_time = self.data_queue.get(block=False)
             self.current_time *= 100
-        except:
+        except queue.Empty:
             self.current_time = 0
 
-    def on_exit(self, evt):
-        """Closes the window.
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(100)
+        self.timer.timeout.connect(self.update_ui)
+        self.timer.start()
+
+    def init_ui(self):
+        """Set up the user interface
         """
-        self.Close()
+        self.widget = QtWidgets.QWidget(self)
+        self.setCentralWidget(self.widget)
 
-    def on_open(self, evt):
-        """Pop up a new dialow window to choose a file, then play the selected file.
+        # In this widget, the video will be drawn
+        if platform.system() == "Darwin": # for MacOS
+            self.videoframe = QtWidgets.QMacCocoaViewContainer(0)
+        else:
+            self.videoframe = QtWidgets.QFrame()
+
+        self.palette = self.videoframe.palette()
+        self.palette.setColor(QtGui.QPalette.Window, QtGui.QColor(0, 0, 0))
+        self.videoframe.setPalette(self.palette)
+        self.videoframe.setAutoFillBackground(True)
+
+        self.vboxlayout = QtWidgets.QVBoxLayout()
+        self.vboxlayout.addWidget(self.videoframe)
+        self.widget.setLayout(self.vboxlayout)
+
+    def open_file(self):
+        """Open a media file in a MediaPlayer
         """
 
-        # Create a file dialog opened in the current home directory, where
-        # you can display all kind of files, having as title "Choose a file".
-        # dlg = wx.FileDialog(self, "Choose a file", os.path.expanduser('~'), "", "*.*", wx.OPEN)
-        dlg = wx.FileDialog(self, message="Choose a media file", defaultDir=os.getcwd(), defaultFile="", style=wx.FD_OPEN | wx.FD_CHANGE_DIR)
+        dialog_txt = "Choose Media File"
+        filename = QtWidgets.QFileDialog.getOpenFileName(self, dialog_txt, os.path.expanduser('~'))
+        if not filename:
+            return
 
-        if dlg.ShowModal() == wx.ID_OK:
-            dirname = dlg.GetDirectory()
-            filename = dlg.GetFilename()
+        # getOpenFileName returns a tuple, so use only the actual file name
+        self.media = self.instance.media_new(filename[0])
 
-            # Create
-            self.media = self.instance.media_new(os.path.join(dirname, filename))
-            self.player.set_media(self.media)
+        # Put the media in the media player
+        self.mediaplayer.set_media(self.media)
 
-            # Report the title of the file chosen
-            title = self.player.get_title()
+        # Parse the metadata of the file
+        self.media.parse()
 
-            # If an error was encountred while retriving the title, then use filename
-            if title == -1:
-                title = filename
+        # Set the title of the track as the window title
+        self.setWindowTitle("{}".format(self.media.get_meta(0)))
 
-            self.SetTitle("{}".format(title))
+        # The media player has to be 'connected' to the QFrame (otherwise the
+        # video would be displayed in it's own window). This is platform
+        # specific, so we must give the ID of the QFrame (or similar object) to
+        # vlc. Different platforms have different functions for this
+        if platform.system() == "Linux": # for Linux using the X Server
+            self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
+        elif platform.system() == "Windows": # for Windows
+            self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
+        elif platform.system() == "Darwin": # for MacOS
+            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
 
-            # set the window id where to render VLC's video output
-            handle = self.videopanel.GetHandle()
-            if platform.system() == "Linux":
-                self.player.set_xwindow(handle)
-            elif platform.system() == "Windows":
-                self.player.set_hwnd(handle)
-            elif platform.system() == "Darwin":
-                self.player.set_nsobject(handle)
+        # Start playing the video as soon as it loads
+        self.mediaplayer.play()
 
-            # Start playing the video as soon as it loads.
-            self.player.play()
-
-        # finally destroy the dialog
-        dlg.Destroy()
-
-    def on_timer(self, evt):
+    def update_ui(self):
         try:
-            current_time = self.data_q.get(block=False)
+            current_time = self.data_queue.get(block=False)
             if current_time != self.current_time:
                 self.current_time = current_time
                 update = True
             else:
                 update = False
-        except:
-            if self.player.is_playing():
-                self.player.pause()
+        except queue.Empty:
+            if self.mediaplayer.is_playing():
+                self.mediaplayer.pause()
             return
 
-        if not self.player.is_playing():
-            self.player.set_time(self.current_time*100)
-            self.player.play()
+        if not self.mediaplayer.is_playing():
+            self.mediaplayer.set_time(self.current_time*100)
+            self.mediaplayer.play()
         elif update:
-            self.player.set_time(self.current_time*100)
-
+            self.mediaplayer.set_time(self.current_time*100)
 
 def main():
+    """Entry point for our simple vlc player
+    """
+    app = QtWidgets.QApplication(sys.argv)
 
-    # Create a wx.App(), which handles the windowing system event loop
-    app = wx.App()
+    data_queue = queue.Queue()
+    _ = Client("localhost", 10000, data_queue)
 
-    q = queue.Queue()
-    client = Client("localhost", 10000, q)
+    player = MiniPlayer(data_queue)
 
-    # Create the window containing our small media player
-    player = MiniPlayer("Simple PyVLC Player", q)
-
-    # Show the player window centred and run the application
-    player.Centre()
-    player.Show()
-    app.MainLoop()
+    player.show()
+    player.resize(480, 480)
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
