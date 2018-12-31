c0b8660ec46d01c0d09a50f625ca668e5c4da242
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2018-12-31 18:12:10 -0500

    Added statusbar & ability to track current behavior in statusbar

--- gui/pyqt_wrapper.py
+++ gui/pyqt_wrapper.py
@@ -20,7 +20,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.plot_window = PlotWindow(data_q, plots, plot_names, beh_intervals=beh_intervals, parent=self)
         self.setCentralWidget(self.plot_window)
 
-        # self.statusBar().showMessage("behavior x")
+        self.statusbar = self.statusBar()
+        self.statusbar.showMessage("Ready")
 
         menu_bar = self.menuBar()
 
@@ -74,8 +75,11 @@ class MainWindow(QtWidgets.QMainWindow):
 class PlotWindow(pg.GraphicsWindow):
     """This class holds plots and should be nested in a MainWindow."""
 
+    changed_behavior = QtCore.pyqtSignal('QString')
+
     def __init__(self, data_q, plots, plot_names, beh_intervals=None, parent=None):
         super().__init__(parent=parent)
+        self.parent = parent
 
         self.graphics_layout = pg.GraphicsLayout(border=(0, 0, 0))
         self.graphics_layout.layout.setSpacing(0)
@@ -87,10 +91,15 @@ class PlotWindow(pg.GraphicsWindow):
         self.timer.start()
         self.timer.timeout.connect(self.on_new_data)
 
+        self.behavior_time = {}
         self.plot_items = []
         self.vertical_lines = []
         self.plots = plots
         self.add_plots(plot_names, beh_intervals)
+       
+        # Convert all sets to strings
+        self.behavior_time = {time: ', '.join(behavior) for time, behavior in self.behavior_time.items()}
+
         self.data_q = data_q
 
     def on_new_data(self):
@@ -99,11 +108,15 @@ class PlotWindow(pg.GraphicsWindow):
         try:
             val = self.data_q.get(block=False)
         except queue.Empty:
+            QtCore.QCoreApplication.processEvents()
             return
+
+        self.parent.statusbar.showMessage(self.behavior_time[val])
 
         for v_line in self.vertical_lines:
             v_line.setValue(val)
-            # QtCore.QCoreApplication.processEvents()
+
+        QtCore.QCoreApplication.processEvents()
 
     def add_plots(self, plot_names, all_beh_intervals):
         """Creates and set all the plots"""
@@ -115,13 +128,21 @@ class PlotWindow(pg.GraphicsWindow):
         pen = pg.mkPen('r', width=2)
 
         for i, plot in enumerate(self.plots):
-            plot_item.setRange(xRange=[0, x_max], yRange=[0, y_max], padding=0)
             plot_item = self.graphics_layout.addPlot(title="plot {}".format(plot_names[i]), row=i, col=0)
 
             # Add background color(s) (color coded by behavior) to each plot
             if all_beh_intervals:
-                for behavior_intervals, color in all_beh_intervals:
+                for behavior_intervals, behavior, color in all_beh_intervals:
                     for interval in behavior_intervals:
+
+                        # Add the interval times and the corresponding behavior
+                        # to track it for the status bar
+                        for time in interval:
+                            if time not in self.behavior_time:
+                                self.behavior_time[time] = {behavior}
+                            else:
+                                self.behavior_time[time].add(behavior)
+
                         rgn = pg.LinearRegionItem(values=[interval[0], interval[-1]], movable=False)
                         rgn.lines[0].setPen((255, 255, 255, 5))
                         rgn.lines[1].setPen((255, 255, 255, 5))
