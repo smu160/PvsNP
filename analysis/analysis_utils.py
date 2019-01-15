82fb6bf1e76009e52a88a12ef6f4c11c8f4303c8
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2019-01-15 12:26:20 -0500

    Fixed bug in define_immobility, generalized z_score, added error checking to distance_moved, and made module compliant with PEP 8

--- analysis/analysis_utils.py
+++ analysis/analysis_utils.py
@@ -1,155 +1,194 @@
 """
-This module contains all the functions necessary for
-feature engineering, data visualization, and data analysis.
+This module contains all the functions necessary for data preprocessing as well
+as data wrangling.
 
 @authors: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
           Jack Berry, Columbia University, jeb2242@columbia.edu
 """
+
 import os
-import sys
 import warnings
-import math
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
+
 
 def preprocess_behavior(etho_filepath, observer_filepath):
     """
     Processes original ethovision and observer files (converted to .csv format)
-    
+
     Args:
-        etho_filepath: file path to raw ethovision behavior .csv file (only 1 sheet)
-        observer_filepath: file path to raw observer .csv file
-    
-    Returns: 
-        behavior dataframe 
-    
+        etho_filepath: str
+            The file path to raw ethovision behavior .csv file (only 1 sheet).
+
+        observer_filepath: str
+            The file path to the raw observer .csv file.
+
+    Returns:
+        behavior: DataFrame
+            The preprocessed behavior DataFrame.
     """
-    #read ethovision data and rename columns
-    nrowsheader = pd.read_csv(etho_filepath, nrows=0, encoding = 'latin1',header=0)
-    nrowsheader=int(nrowsheader.columns[1])
 
-    behavior=pd.read_csv(etho_filepath, header=nrowsheader-2, encoding = 'latin1')
-    behavior=behavior.drop(behavior.index[0])
+    # read ethovision data and rename columns
+    nrowsheader = pd.read_csv(etho_filepath, nrows=0, encoding="latin1", header=0)
+    nrowsheader = int(nrowsheader.columns[1])
 
-    #rename columns
-    new_cols=[]
-    for s in behavior.columns:
-        s=s.replace(" ","_")
-        s=s.replace("In_zone(","")
-        s=s.replace("_/_center-point)","")
-        new_cols.append(s)
-    behavior.columns=new_cols
-    
-    #read observer data and format
-    
+    behavior = pd.read_csv(etho_filepath, header=nrowsheader-2, encoding="latin1")
+    behavior = behavior.drop(behavior.index[0])
+
+    # rename columns
+    new_cols = []
+    for col in behavior.columns:
+        col = col.replace(' ', '_')
+        col = col.replace("In_zone(", '')
+        col = col.replace("_/_center-point)", '')
+        new_cols.append(col)
+
+    behavior.columns = new_cols
+
+    # read observer data and format
     obs = pd.read_csv(observer_filepath)
-    obs=obs.drop(['Observation','Event Log',"Time"],axis=1)
-    obs.index=obs.index+1
-    obs.fillna(0,inplace=True)
-    
-    #merge behavior and observer dataframes, and return result
-    num_obs = len(obs.columns) #number of hand-scored observations
+    obs = obs.drop(["Observation", "Event Log", "Time"], axis=1)
+    obs.index += 1
+    obs.fillna(0, inplace=True)
+
+    # merge behavior and observer dataframes, and return result
+    num_obs = len(obs.columns) # number of hand-scored observations
     obs[obs != 0] = 1
-    behavior=pd.merge(behavior,obs,how='left',left_index=True,right_index=True)
+    behavior = pd.merge(behavior, obs, how="left", left_index=True, right_index=True)
     behavior.update(behavior[behavior.columns[-num_obs:]].fillna(0))
-    
+
     return behavior
 
-def z_score_data(data):
+
+def z_score_data(data, mew=None):
     """This function simply z scores all the given neural signal data.
 
     Args:
-        data:
-
+        data: DataFrame
             Ca2 transient data in T x N format, where N is the # of neuron
             column vectors, and T is the number of observations (rows),
             all in raw format.
 
+        mew: int, optional, default: None
+            The mean value for the baseline of the data.
+            Note: if the raw data comes from CNMF_E, use 0 for the baseline.
+
     Returns:
         z_scored_data: DataFrame
-
             The z scored raw cell data in T x N format.
     """
     pop_offset = np.percentile(data.values, 50)
     sigma = np.std(data.values, axis=0)
-    #mew = np.mean(data.values[data.values < pop_offset])
-    mew = 0 #if raw data comes from CNMFE, use 0 for baseline
-    return pd.DataFrame(data=((data.values - mew) / sigma))
 
-def pairwise_dist(x):
-    """
-    Helper function for distance_moved. Computes consecutive pairwise differences in a Series
-    
+    if mew is None:
+        mew = np.mean(data.values[data.values < pop_offset])
+    else:
+        mew = mew
+
+    z_scored_data = pd.DataFrame(data=((data.values - mew) / sigma))
+    return z_scored_data
+
+
+def pairwise_dist(x_coords):
+    """Computes consecutive pairwise differences in a Series.
+
+    NOTE: This is a helper function for distance_moved.
+
     Input:
-        x: Series containing floats
-        
-    Returns:
-        dx: Series containing x[i] - x[i-1]
-    
-    """
-    
-    x = x.astype(float)
-    dx = (np.roll(x,-1) - x).shift(1)
-    return dx
+        x_coords: pandas Series
+            A one-dimensional ndarray of x coordinates, over time.
 
-def distance_moved(x,y):
+    Returns:
+        dx: pandas Series
+            A series containing all the values x[i] - x[i-1]
+
     """
-    Inputs
-        x,y: Series containing x and y positions over time
-    
+    x_coords = x_coords.astype(float)
+    delta_x = (np.roll(x_coords, -1) - x_coords).shift(1)
+    return delta_x
+
+
+def distance_moved(x_coords, y_coords):
+    """
+    Args:
+        x_coords: pandas Series
+            A one-dimensional ndarray of x coordinates, over time.
+
+        y_coords:
+            A one-dimensional ndarray of y coordinates, over time.
+
     Returns
         Series containing distance moved per frame
-    
-    """  
-    
-    dx = pairwise_dist(x.astype(float))
-    dy = pairwise_dist(y.astype(float))
-    
-    dist_moved = dx**2 + dy**2
-    
-    return dist_moved.apply(math.sqrt)
 
-def compute_velocity(dist,fr=10):
     """
-    Inputs
-        dist: Series containing distance moved
-        fr: frame rate
-    Returns
-        Series containing velocity 
-    
-    """
-    
-    return dist.apply(lambda x: x*fr)
+    if len(x_coords) != len(y_coords):
+        raise ValueError("x_coords and y_coords are not of equal length!")
 
-def define_immobility(velocity, min_dur = 1, min_vel = 2, fr = 10, min_periods = 1):
+    delta_x = pairwise_dist(x_coords.astype(float))
+    delta_y = pairwise_dist(y_coords.astype(float))
+
+    dist_moved = delta_x**2 + delta_y**2
+    dist_moved = dist_moved.apply(np.sqrt)
+    return dist_moved.apply(np.sqrt)
+
+
+def compute_velocity(dist_moved, framerate=10):
     """
-    Defines time periods of immobility based on a rolling window of velocity.
-    Mouse is considered immobile if velocity has not exceeded min_vel for the previous min_dur seconds
-    Defaults for min_dur and min_vel are taken from Stefanini...Fusi et al. 2018
-    
-    Inputs:
-        velocity: Series with velocity data
-        min_dur: minimum length of time in seconds in which velocity must be low. Default is 1 second.
-        min_vel: minimum velocity in cm/s for which the mouse can be considered mobile. Default is 2 cm/s
-        fr: frame rate of velocity Series. Default is 10 fps.
-        min_periods: Minimum number of datapoints needed to determine immobility. Default is 1.
-            This value is needed to define immobile time bins at the beginning of the session. If 
-            min_periods = 8, then the first 8 time bins will be be considered immobile, regardless of velocity
-    
+    Args:
+        dist_moved: pandas Series
+            A one-dimensional ndarray containing distance moved.
+
+        framerate: int, optional, default: 10
+            The frame rate corresponding to the dist_moved Series.
+
     Returns:
-        Series defining immobile (1) and mobile (0) times
-    
+        velocity: pandas Series
+            A one-dimensional ndarray containing velocity.
     """
-    
-    window_size = fr*min_dur
-    rolling_max_vel = velocity.rolling(window_size,min_periods=1).max()
+    velocity = dist_moved.apply(lambda x: x * framerate)
+    return velocity
 
-    return (rolling_max_vel<min_vel).astype(int)
 
-    
+def define_immobility(velocity, min_dur=1, min_vel=2, framerate=10, min_periods=1):
+    """Define time periods of immobility based on a rolling window of velocity.
 
+    A Mouse is considered immobile if velocity has not exceeded min_vel for the
+    previous min_dur seconds.
+
+    Default values for min_dur and min_vel are taken from:
+    Stefanini...Fusi et al. 2018 (https://doi.org/10.1101/292953)
+
+    Args:
+        velocity: pandas Series
+            A one-dimensional ndarray of the velocity data.
+
+        min_dur: int, optional, default: 1
+            The minimum length of time in seconds in which velocity must be low.
+
+        min_vel: int, optional, default: 2
+            The minimum velocity in cm/s for which the mouse can be considered
+            mobile.
+
+        framerate: int, optional, default: 10
+            The frame rate of the velocity Series. Default is 10 fps.
+
+        min_periods: int, optional, default: 1
+            Minimum number of datapoints needed to determine immobility. This
+            value is needed to define immobile time bins at the beginning of the
+            session. If min_periods=8, then the first 8 time bins will be be
+            considered immobile, regardless of velocity.
+
+    Returns:
+        mobile_immobile: pandas Series
+            A one-dimensional ndarray of 0's and 1's, where 1 signifies immobile
+            times and 0 signifies mobile times.
+
+    """
+    window_size = framerate * min_dur
+    rolling_max_vel = velocity.rolling(window_size, min_periods=min_periods).max()
+    mobile_immobile = (rolling_max_vel < min_vel).astype(int)
+
+    return mobile_immobile
 
 
 def find_file(root_directory, target_file):
@@ -171,36 +210,41 @@ def find_file(root_directory, target_file):
     root_directory = os.path.join(os.path.expanduser("~"), root_directory)
 
     if not os.path.exists(root_directory):
-        print("{} does not exist!".format(root_directory), file=sys.stderr)
-        return
+        raise FileNotFoundError("{} does not exist!".format(root_directory))
     if not os.path.isdir(root_directory):
-        print("{} is not a directory!".format(root_directory), file=sys.stderr)
-        return
+        raise NotADirectoryError("{} is not a directory!".format(root_directory))
 
-    for subdir, dirs, files in os.walk(root_directory):
+    for subdir, _, files in os.walk(root_directory):
         for file in files:
             if file == target_file:
                 file_path = os.path.join(subdir, file)
                 return file_path
 
-    print("{} not found!".format(target_file), file=sys.stderr)
+    raise FileNotFoundError("{} not found!".format(target_file))
+
 
 def extract_epochs(mouse, behavior):
     """Extract all epochs of continuous behaviors/events.
 
     Args:
         mouse: Mouse
+            An instance of the Mouse class that has a `spikes_and_beh`
+            pandas DataFrame, where `spikes_and_beh` has neural activity
+            and corresponding behavior concatenated.
 
         behavior: str
+            The name of the behavior to use when extracting continuous time
+            periods/events.
 
     Returns:
-        df: DataFrame
+        epochs_df: DataFrame
 
     """
     dataframe = mouse.spikes_and_beh.copy()
     dataframe["block"] = (dataframe[behavior].shift(1) != dataframe[behavior]).astype(int).cumsum()
-    df = dataframe.reset_index().groupby([behavior, "block"])["index"].apply(np.array)
-    return df
+    epochs_df = dataframe.reset_index().groupby([behavior, "block"])["index"].apply(np.array)
+    return epochs_df
+
 
 def filter_epochs(interval_series, framerate=10, seconds=1):
     """Helper function for extract_epochs.
@@ -208,9 +252,9 @@ def filter_epochs(interval_series, framerate=10, seconds=1):
     Args:
         interval_series: list
 
-        framerate: int, optional
+        framerate: int, optional, default: 10
 
-        seconds: int, optional
+        seconds: int, optional, default: 1
 
     Returns:
         intervals: list
@@ -225,7 +269,8 @@ def filter_epochs(interval_series, framerate=10, seconds=1):
 
     return intervals
 
-class Mouse(object):
+
+class Mouse:
     """A base class for keeping all relevant & corresponding objects, i.e.,
     spikes, cell transients, & behavior dataframes, with their respective
     mouse.
@@ -242,8 +287,9 @@ class Mouse(object):
             message = "A behavior dataframe was not provided."
             warnings.warn(message, Warning)
 
-        # Adds "Running_frames" column to the end of the behavior Dataframe
         velocity_cutoff = kwargs.get("velocity_cutoff", None)
+
+        # Adds "Running_frames" column to the end of the behavior Dataframe
         if velocity_cutoff is not None:
             running_frames = np.where(self.behavior["Velocity"] > velocity_cutoff, 1, 0)
             self.behavior = self.behavior.assign(Running_frames=running_frames)
@@ -254,22 +300,24 @@ class Mouse(object):
 
         Args:
             dataframe: DataFrame
-                the pandas DataFrame to be downsampled
+                The pandas DataFrame to be downsampled
 
             row_multiple: int
-                the row multiple is the rows to be removed.
-                e.g., row_multiple of 3 would remove every 3rd row from the
+                The row multiple is the rows to be removed,
+                e.g., a row_multiple of 3 would remove every 3rd row from the
                 provided dataframe
 
         Returns:
             dataframe: DataFrame
-                the downsampled pandas DataFrame
+                The downsampled pandas DataFrame.
         """
+
         # Drop every nth (row multiple) row
         dataframe = dataframe.iloc[0::row_multiple, :]
 
         # Reset and drop the old indices of the pandas DataFrame
         dataframe.reset_index(inplace=True, drop=True)
+
         return dataframe
 
 
@@ -311,4 +359,3 @@ class Mouse(object):
             #else:
                 #raise ValueError("{} is not a column in the provided dataframe.".format(behavior))
         return activity_df
-
