cfa2db239082bc1bc1a7dd8a0a12078e2791336a
Jack Berry <jeb2242@columbia.edu>
2019-01-17 11:19:25 -0500

    added functions for MI and pearson correlation

--- analysis/clustering.py
+++ analysis/clustering.py
@@ -1,4 +1,74 @@
+"""
+This module contains all the functions necessary for clustering.
+
+@authors: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
+          Jack Berry, Columbia University, jeb2242@columbia.edu
+"""
+
+
 from sklearn import cluster
+from sklearn.metrics import normalized_mutual_info_score
+from sklearn.metrics import mutual_info_score
+import itertools
+import pandas as pd
+import numpy as np
+
+def compute_nmi(dataframe):
+    connections = {}
+    matrix = nmi_matrix(dataframe)
+
+    for combination in itertools.combinations(matrix.columns, 2):
+        nmi = matrix[combination[0]][combination[1]]
+        connections[combination] = nmi
+
+    return connections
+
+def nmi_matrix(dataframe):
+    rows = []
+    
+    for col in dataframe:
+        row = dataframe.apply(normalized_mutual_info_score, args=(dataframe[col],))
+        rows.append(row)
+
+    return pd.DataFrame(rows, index=dataframe.columns) 
+
+def compute_mi(dataframe):
+    connections = {}
+    matrix = mi_matrix(dataframe)
+
+    for combination in itertools.combinations(matrix.columns, 2):
+        mi = matrix[combination[0]][combination[1]]
+        connections[combination] = mi
+
+    return connections
+
+def mi_matrix(dataframe):
+    rows = []
+    
+    for col in dataframe:
+        row = dataframe.apply(mutual_info_score, args=(dataframe[col],))
+        rows.append(row)
+
+    return pd.DataFrame(rows, index=dataframe.columns)
+
+
+
+def compute_corrcoef(dataframe, threshold = 0):
+    connections = {}
+    
+    for neuron_pair in itertools.combinations(dataframe.columns, 2):
+        if dataframe[neuron_pair[0]].std() == 0 or dataframe[neuron_pair[1]].std() == 0:
+            continue
+
+        corrcoef = np.corrcoef(dataframe[neuron_pair[0]].values, dataframe[neuron_pair[1]].values)[0][1]
+        if corrcoef >= threshold:
+            connections[neuron_pair] = corrcoef
+        
+    return connections
+
+
+
+
 
 def affinity_propagation(similiarity_matrix):
     """Perform Affinity Propagation Clustering of data
