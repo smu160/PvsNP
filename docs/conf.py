2b5f818d6434b681e46dcf9dd52d2053d7504982
Saveliy Yusufov <saveliy.m.yusufov@gmail.com>
2019-04-08 20:35:07 -0400

    :butterfly: Beautified docs using the NumPy stle

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../analysis'))
+sys.path.insert(0, os.path.abspath('../pvsnp'))
 
 
 # -- Project information -----------------------------------------------------
